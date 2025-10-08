db12b179af97f12016168c551152baa6f412232f
kidus <kidus6135@gmail.com>
2025-10-08 11:27:03 +0300

    add testimonial components

---
+++ src/Componenets/testimonials/Testimonials.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import landscape from "../../assets/landscape.jpg";
+import slovenia from "../../assets/slovenia.jpg";
+import sunset from "../../assets/sunset.jpg";
+import "./Testimonials.css";
+
+function Testimonial() {
+  const slides = [
+    { img: landscape, name: "Israel", text: "The travel agency gave me the best trip" },
+    { img: slovenia, name: "Eyu", text: "The travel agency is the best" },
+    { img: sunset, name: "Kira", text: "I've had the best trip ever" }
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  const nextSlide = () => {
+    setIndex((prev) => (prev + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+
+  return (
+    <div className="testimony-section">
+      <h2 className="test-title">Popular Destinations</h2>
+
+      <div className="testimony-slide">
+        <div className="test-card">
+          <img src={slides[index].img} alt={slides[index].name} />
+          <div className="test-info">
+            <h3>{slides[index].name}</h3>
+            <p>{slides[index].text}</p>
+          </div>
+        </div>
+      </div>
+      <div className="btn">
+        <button className="prev" onClick={prevSlide}>Previous</button>
+        <button className="next"onClick={nextSlide}>Next</button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Testimonial;
